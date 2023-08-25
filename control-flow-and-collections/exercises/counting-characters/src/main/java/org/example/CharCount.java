a98efdf9d09837a68c92f06c0de01089742267c5
Austin <kenjigw@gmail.com>
2023-08-24 20:36:44 -0500

    finished bonus

--- control-flow-and-collections/exercises/counting-characters/src/main/java/org/example/CharCount.java
+++ control-flow-and-collections/exercises/counting-characters/src/main/java/org/example/CharCount.java
@@ -1,29 +1,59 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
 public class CharCount {
 
     public static void main(String[] args) {
+        String fileName = "C:\\Users\\Avari\\Desktop\\LC Project\\javaDevProjects\\control-flow-and-collections\\exercises\\counting-characters\\src\\main\\java\\org\\example\\test.txt";
 
-        Scanner input = new Scanner(System.in);
+        try {
+            File newFile = new File(fileName);
+            Scanner scanner = new Scanner(newFile);
 
-        System.out.println("Input words.");
-        String userText = input.nextLine();
+            StringBuilder content = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                content.append(scanner.nextLine()).append("\n");
+            }
+            String fileContent = content.toString();
+            char[] charactersInString = fileContent.toLowerCase().toCharArray();
+
+            HashMap<Character, Integer> charCounts = new HashMap<>();
+
+            for (Character c : charactersInString) {
+                if(!Character.isDigit(c))charCounts.put(c, charCounts.getOrDefault(c, 0) +1);
+            }
+
+            for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+           scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+//        System.out.println("Input words.");
+//        String userText = input.nextLine();
 
 //        String newText = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
-        char[] charactersInString = userText.toLowerCase().toCharArray();
-
-        HashMap<Character, Integer> charCounts = new HashMap<>();
-
-        for (Character c : charactersInString) {
-            if(!Character.isDigit(c))charCounts.put(c, charCounts.getOrDefault(c, 0) +1);
-        }
-
-        for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+//        char[] charactersInString = fileContent.toLowerCase().toCharArray();
+//
+//        HashMap<Character, Integer> charCounts = new HashMap<>();
+//
+//        for (Character c : charactersInString) {
+//            if(!Character.isDigit(c))charCounts.put(c, charCounts.getOrDefault(c, 0) +1);
+//        }
+//
+//        for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
     }
 }
